b2e3b2276ba31591dba01e9244a2f99e3a32ec39
Anton <borzenkov0anton@gmail.com>
2023-09-11 19:34:00 +0200

    styled header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
 export const Navbar = () => {
   return (
-    <div className='navbar'>
-        <div className='container'>
-          <div className='n-container'>
-            <div className='n-logo'>
-              <span>Anton Barziankou</span>
+    <div className="navbar">
+      <div className="container">
+        <div className="n-container">
+          <div className="n-logo">
+            <span>Anton Barziankou</span>
+          </div>
+          <div className="n-right">
+            <div className="n-menu">
+              <span>What we do</span>
+              <span>How it works</span>
+              <span>Who we invest in </span>
+              <span>Test it!</span>
             </div>
-            <div className='n-right'>
-              right
+            <div className="fund-button">
+              <button>button</button>
             </div>
           </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
